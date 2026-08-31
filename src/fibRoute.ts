184fb33a805117581e6c075095b07d275317927c
jana-hussien <jhussien@andrew.cmu.edu>
2026-08-31 15:47:17 +0300

    Translate fibRoute.ts to TypeScript

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,14 +1,10 @@
 // Endpoint for querying the fibonacci numbers
-
 import { Request, Response } from "express";
-import * as fibonacciModule from "./fib";
-
-const fibonacci = fibonacciModule as unknown as (n: number) => number;
+import fib from "./fib";
 
 export default (req: Request, res: Response): void => {
   const { num } = req.params as { num: string };
-
-  const fibN = fibonacci(Number(num));
+  const fibN = fib(Number(num));
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
